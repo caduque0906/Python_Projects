d6a8d5ce39a67d5364439a641aec67b4e54c1127
caduque0906 <caduque0906@gmail.com>
2023-06-07 09:08:20 -0400

    first commit

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+class Trainer:
+    name = 'No Name Provided'
+    elementType = 'Fire'
+    gymBadges = 2
+
+class Charizard (Trainer):
+    move_amount = 5
+    specialAttack = 'Flare Blitz'
+    No = 6
+
+class HoOh (Trainer):
+    move_amount = 4
+    specialAttack = 'Sacred Fire'
+    No = 250
