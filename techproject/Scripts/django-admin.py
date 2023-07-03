c0c0550131370f952908e8163bec800cc80531c0
caduque0906 <caduque0906@gmail.com>
2023-07-03 14:42:47 -0400

    First commit

---
+++ techproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Carlos\Documents\Github\Python_Projects\techproject\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
