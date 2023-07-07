1d458d5ff8bd7214216b0d706bcdb65281dbe5bb
caduque0906 <caduque0906@gmail.com>
2023-07-06 21:11:04 -0400

    Added __str__

--- DjangoUniversity/Classes/campusApp/models.py
+++ DjangoUniversity/Classes/campusApp/models.py
@@ -8,6 +8,13 @@ class UniversityCampus(models.Model):
     # Creates model manager
     object = models.Manager()
 
+    #Displays the object output values in the form of a string
+    def __str__(self):
+        # Returns the input value of the title and instructor name
+        # field as a tuple to display in the browser instead of the default titles
+        display_course = '{0.campus}'
+        return display_course.format(self)
+
     # Removes added 's' that Django adds to the model name in the browser display
     class Meta:
         verbose_name_plural = "University Campus"
